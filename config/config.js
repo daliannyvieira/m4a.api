737b34912544ff9b0a782d9c8ed488deb05083a5
daliannyvieira <vieiradalianny@gmail.com>
2019-08-13 19:58:34 -0300

    fix env issue

--- config/config.js
+++ config/config.js
@@ -2,11 +2,20 @@ require('dotenv').config()
 require('dotenv').load()
 
 module.exports = {
-  "development": {
+  "localhost": {
     "username": process.env.LOCAL_RELACIONAL_DB_USERNAME,
     "password": process.env.LOCAL_RELACIONAL_DB_PASS,
     "database": process.env.LOCAL_RELACIONAL_DB_NAME,
     "host": process.env.LOCAL_RELACIONAL_DB_HOST,
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "jwtSecret": process.env.JWT_SECRET,
+    "dialect": "mysql"
+  },
+  "development": {
+    "username": process.env.RELACIONAL_DB_USERNAME,
+    "password": process.env.RELACIONAL_DB_PASS,
+    "database": process.env.RELACIONAL_DB_NAME,
+    "host": process.env.RELACIONAL_DB_HOST,
     "projectId": process.env.FIREBASE_PROJECT_ID,
     "jwtSecret": process.env.JWT_SECRET,
     "dialect": "mysql"
